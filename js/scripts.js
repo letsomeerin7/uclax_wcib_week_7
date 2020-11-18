ce9867e009489bb7675cc6ee6942dcc7814a92b4
letsomeerin7 <erinjamison@erins-mbp.home>
2020-11-18 03:17:24 -0800

    ecard

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
 jQuery(function() {
 
+  let my_switch = false;
+
 	$('#my_button').click(function () {
+
+		if (my_switch == false) {
+			my_switch = true;
+
+			$(this).html('Surprise!');
+
+			$('#my_image').attr('src', 'images/samson-puppy.jpg');
+		} else {
+			my_switch = false;
+
+			$(this).html("What's Coming?");
+
+			$('#my_image').attr('src', 'images/balloons.png');
+		}
+
 	});
+
+	$('#alt_button').mouseenter(function(){
+  alert("IT'S A PUPPY! (So much better than a baby, amiright?)");
+});
 
 });
